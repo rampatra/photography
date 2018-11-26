42b744605ccd55b3b8d1f8e92a3e6ec26d36aed6
Chip Oglesby <chip@creativesparkcolumbia.com>
2018-11-25 22:25:58 -0500

    Update npmfile.js

--- npmfile.js
+++ npmfile.js
@@ -1,3 +1,3 @@
 exports.printMsg = function() {
-    console.log("Visit http://photography.ramswaroop.me for a treat!");
+    console.log("Visit http://photography.chipoglesby.com for a treat!");
 };
