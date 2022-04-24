0eadad58b29771922472ec892ee9e026cef59599
Banipreet Singh Raheja <banipreet.raheja@gmail.com>
2022-04-24 20:03:45 +0100

    Update author information and social media links

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@
  html5up.net | @ajlkn
  Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 
- Added EXIF data and enhanced for Jekyll by Ram Patra
+ Added EXIF data and enhanced for Jekyll by Banipreet
  */
 
 (function ($) {
