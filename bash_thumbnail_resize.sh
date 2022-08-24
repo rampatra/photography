17c827d973196228983bbcb7cfe1debd0af26a14
Andrew Sivaprakasam <andrew.n.sivaprakasam@gmail.com>
2022-08-24 10:24:50 -0400

    updated with bash script, master

--- bash_thumbnail_resize.sh
+++ bash_thumbnail_resize.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
 cp images/* images/fulls
-cp images/* images/thumbs
-for f in /images/thumbs/; do convert "$f" -resize 510 "$f"; done 
+cp images/fulls/* images/thumbs
+echo "images transferred"
+rm images/*.jpg
+rm images/*.JPG
+rm images/*.png
+for f in images/thumbs/*; do convert "$f" -resize 510 "$f"; done 
+echo "thumbs generated"
+
